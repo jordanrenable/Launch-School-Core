7bc9fab6f9b0cead1fc37617b36c0c0cae281662
Jordan Able <jordanrenable@gmail.com>
2024-02-27 21:46:44 +0000

    Rock Paper Scissors bonus 3 upgrades

--- rb_101/lesson_2/rps_bonus_3.rb
+++ rb_101/lesson_2/rps_bonus_3.rb
@@ -1,24 +1,53 @@
-CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-VALID_INPUTS = ['r', 'p', 's', 'l', 'sp']
+CHOICES = %w(rock paper scissors lizard spock)
+VALID_INPUTS = %w(r p s l sp)
+WINNING_COMBOS = { 'rock' => %w(scissors lizard),
+                   'paper' => %w(rock spock),
+                   'scissors' => %w(lizard paper),
+                   'lizard' => %w(paper spock),
+                   'spock' => %w(rock scissors) }
 
 def prompt(message)
   puts("=> #{message}")
 end
 
+def win?(first, second)
+  WINNING_COMBOS[first].include?(second)
+end
+
+def winner(player, computer)
+  if win?(player, computer)
+    "You won!"
+  elsif win?(computer, player)
+    "Computer won!"
+  else
+    "It's a tie!"
+  end
+end
+
+prompt(<<~RULES
+      Let's play Rock, Paper, Scissors, Lizard, Spock!
+      Scissors cut Paper
+      Paper covers Rock
+      Rock crushes Lizard
+      Lizard poisons Spock
+      Spock smashes Scissors
+      Scissors decapitate Lizard
+      Lizard eats Paper
+      Paper disproves Spock
+      Spock vaporizes Rock
+      and as it always has been
+      Rock crushes Scissors
+      RULES
+      )
+
 loop do
   choice = ''
 
-  winning_combos = { rock: ['scissors', 'lizard'],
-                     paper: ['rock', 'spock'],
-                     scissors: ['lizard', 'paper'],
-                     lizard: ['paper', 'spock'],
-                     spock: ['rock', 'scissors'] }
-
-  abbreviated_choices = { r: 'rock',
-                          p: 'paper',
-                          s: 'scissors',
-                          l: 'lizard',
-                          sp: 'spock' }
+  abbreviated_choices = { 'r' => 'rock',
+                          'p' => 'paper',
+                          's' => 'scissors',
+                          'l' => 'lizard',
+                          'sp' => 'spock' }
 
   player_wins = 0
   computer_wins = 0
@@ -36,7 +65,7 @@ loop do
         MSG
             )
 
-      choice = gets.chomp
+      choice = gets.chomp.downcase
 
       if VALID_INPUTS.include?(choice)
         break
@@ -45,19 +74,17 @@ loop do
       end
     end
 
-    choice = abbreviated_choices[choice.to_sym]
+    choice = abbreviated_choices[choice]
     computer_choice = CHOICES.sample
 
     prompt("You chose #{choice}; Computer chose #{computer_choice}")
 
-    if winning_combos[choice.to_sym].include?(computer_choice)
-      prompt("You won!")
+    prompt(winner(choice, computer_choice))
+
+    if win?(choice, computer_choice)
       player_wins += 1
-    elsif winning_combos[computer_choice.to_sym].include?(choice)
-      prompt("Computer won!")
+    elsif win?(computer_choice, choice)
       computer_wins += 1
-    else
-      prompt("It's a tie!")
     end
 
     prompt("Score: You- #{player_wins} Computer- #{computer_wins}")
