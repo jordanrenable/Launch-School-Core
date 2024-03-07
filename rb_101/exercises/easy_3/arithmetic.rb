a16f81b2da90523f1b8c9a7112eb88cc137d2ff4
Jordan Able <jordanrenable@gmail.com>
2024-03-07 21:46:57 +0000

    Arithmetic exercise

---
+++ rb_101/exercises/easy_3/arithmetic.rb
@@ -0,0 +1,11 @@
+puts "==> Enter the first positive number:"
+first = gets.to_f
+puts "==> Enter the last positive number:"
+last = gets.to_f
+
+puts "==> #{first} + #{last} = #{first + last}"
+puts "==> #{first} - #{last} = #{first - last}"
+puts "==> #{first} * #{last} = #{first * last}"
+puts "==> #{first} / #{last} = #{first / last}"
+puts "==> #{first} % #{last} = #{first % last}"
+puts "==> #{first} ^ #{last} = #{first**last}"
