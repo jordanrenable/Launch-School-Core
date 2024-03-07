6dc4efe4d103db44ee3a23a9b4398d9a4380e578
Jordan Able <jordanrenable@gmail.com>
2024-03-07 02:01:55 +0000

    Moved exercises to their respective subdirectory

--- rb_101/exercises/easy_2/tip_calculator.rb
+++ rb_101/exercises/easy_2/tip_calculator.rb
@@ -1,9 +1,10 @@
 puts "How much is the bill?"
 bill = gets.to_f.round(2)
 puts "What percentage would you like to tip?"
-tip_percent = gets.to_f.round(2)
-tip = (bill * (tip_percent / 100)).round(2)
+tip_percent = gets.to_f.round(2) / 100
+tip = (bill * tip_percent).round
+
 total = (bill + tip).round(2)
 
-puts "The tip is $#{format('%.2f', tip)}"
-puts "The total is $#{format('%.2f', total)}"
+puts "The tip is $#{tip}"
+puts "The total is $#{total}"
--- rb_101/exercises/repeat_yourself.rb
+++
@@ -1,5 +0,0 @@
-def repeat_yourself(string, integer)
-  integer.times { puts string }
-end
-
-repeat_yourself('Everything is awesome', 10)
