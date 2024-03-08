d080fe7aaec0219d8a88275879b2bfed96a356c4
Jordan Able <jordanrenable@gmail.com>
2024-03-08 01:25:14 +0000

    Short long short exercise

---
+++ rb_110/exercises/easy_4/short_long_short.rb
@@ -0,0 +1,16 @@
+def short_long_short(str1, str2)
+  if str1.size > str2.size
+    long = str1
+    short = str2
+  else
+    long = str2
+    short = str1
+  end
+  short + long + short
+end
+
+puts short_long_short('abc', 'defgh') == "abcdefghabc"
+puts short_long_short('abcde', 'fgh') == "fghabcdefgh"
+puts short_long_short('', 'xyz') == "xyz"
+
+puts short_long_short('banga', 'rang')
